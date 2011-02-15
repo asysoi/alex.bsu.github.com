bbd3d7431f62469c4cf9db1e762bea26f41e2ef5
asysoi <asysoi@ALEX-TP>
2011-02-16 00:31:55 +0200

    second version

--- src/HelloBSU.java
+++ src/HelloBSU.java
@@ -6,7 +6,7 @@ public class HelloBSU {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Sytem.out.println("Welcome ");
+		Sytem.out.println("Welcome BSU");
 	}
 
 }
